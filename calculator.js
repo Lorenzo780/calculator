3b967f26a346e7d4892df276862dcb9d802184e9
Lorenzo780 <65716729+Lorenzo780@users.noreply.github.com>
2020-05-25 21:21:05 +0200

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function insert(num)
+{
+	document.form.textview.value = document.form.textview.value + num;
+}
+function equal()
+{
+	var result = document.form.textview.value;
+	document.form.textview.value = eval(result);
+}
+function clean()
+{
+	document.form.textview.value = "";
+}
+function back()
+{
+	var result = document.form.textview.value;
+	document.form.textview.value = result.substring(0,result.length-1);
+}
